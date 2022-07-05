b989204f7b2c329c7d26442843b12683b3ac001f
MrQuatrelle <maigasmendo@gmail.com>
2022-07-05 09:49:36 +0100

    Initial readme push

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use std::io;
+
 struct Node {
     id: u32,
     parents: Vec<Node>,
 }
 
-fn main() {
-    let _nodes: Vec<Node> = Vec::new();
-    println!("Hello, world!");
+fn parse_input() -> Vec<Vec<u32>> {
+    let mut ret: Vec<Vec<u32>> = Vec::new();
+    return ret;
+}
+
+fn build_tree() -> Vec<Node> {
+    let mut ret: Vec<Node> = Vec::new();
+    return ret;
+}
+
+fn main() -> io::Result<()> {
+    let parsed_input: Vec<Vec<u32>> = parse_input();
+    let _nodes: Vec<Node> = build_tree();
+    Ok(())
 }
